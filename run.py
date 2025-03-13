4c33be4be071c0dd5ddc649d2c13dfe8945ca45e
3DUCK <kermit7520k@gmail.com>
2025-03-14 01:28:06 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app()  # 실제 애플리케이션 인스턴스 생성
+app = create_app(host="0.0.0.0", port=5000, debug=True)  # 실제 애플리케이션 인스턴스 생성
 
 if __name__ == "__main__":
     app.run()
